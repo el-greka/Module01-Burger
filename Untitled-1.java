7e14b4c95ba9006c0c189a5fd4daf34ddf4801cb
el-greka <lenagkir@gmail.com>
2023-02-23 21:14:45 +0100

    Release 0

---
+++ Untitled-1.java
@@ -0,0 +1,11 @@
+
+public static void main(String[]arg)
+string name="Lena";
+int mounth=5;
+double favorite =3.18;
+System.out.println("Hi,my name is %s, my favorite mounth is %d, my favorite double is %f",name,mounth,favorite);
+
+
+} {
+    
+}
